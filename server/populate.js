9bb94127b8dbf2009a01661925c8216916688f18
Luka <pro.chikvaidze@gmail.com>
2023-10-05 22:29:26 +0400

    fix6

--- server/populate.js
+++ server/populate.js
@@ -1,3 +1,4 @@
+const { default: axios } = require("axios")
 const {HomeFurnitureModel, ElectronicsModel, ClothingFashionModel, AccessoriesModel, SelfCareModel, FoodBeverageModel, CarsPartsModels, AllProductsModel} = require("./models/product")
 
 
@@ -5,57 +6,57 @@ const {HomeFurnitureModel, ElectronicsModel, ClothingFashionModel, AccessoriesMo
 const populate_data = async () => {
     try {
         //Populate Electronics data
-        const requestSmartphones = await fetch('https://dummyjson.com/products/category/smartphones')
+        const requestSmartphones = await axios.get('https://dummyjson.com/products/category/smartphones')
         const smartphoneData = await requestSmartphones.json().then(prod => prod)
-        const requestLaptops = await fetch('https://dummyjson.com/products/category/laptops')
+        const requestLaptops = await axios.get('https://dummyjson.com/products/category/laptops')
         const requestLaptopsData = await requestLaptops.json().then(prod => prod)
 
         //Populate Fashion & Clothing data
-        const requestFragnances = await fetch('https://dummyjson.com/products/category/fragnances')
+        const requestFragnances = await axios.get('https://dummyjson.com/products/category/fragnances')
         const fragnancesData = await requestFragnances.json().then(prod => prod)
-        const requestTops = await fetch('https://dummyjson.com/products/category/tops')
+        const requestTops = await axios.get('https://dummyjson.com/products/category/tops')
         const topsData = await requestTops.json().then(prod => prod)
-        const requestWomenDresses = await fetch('https://dummyjson.com/products/category/womens-dresses')
+        const requestWomenDresses = await axios.get('https://dummyjson.com/products/category/womens-dresses')
         const WomenDressesData = await requestWomenDresses.json().then(prod => prod)
-        const requestWomenShoes = await fetch('https://dummyjson.com/products/category/womens-shoes')
+        const requestWomenShoes = await axios.get('https://dummyjson.com/products/category/womens-shoes')
         const womenShoesData = await requestWomenShoes.json().then(prod => prod)
-        const requestMensShirts = await fetch('https://dummyjson.com/products/category/mesns-shirts')
+        const requestMensShirts = await axios.get('https://dummyjson.com/products/category/mesns-shirts')
         const mensShirtsData = await requestMensShirts.json().then(prod => prod)
-        const requestMensShoes = await fetch('https://dummyjson.com/products/category/mens-shoes')
+        const requestMensShoes = await axios.get('https://dummyjson.com/products/category/mens-shoes')
         const meshShoesData = await requestMensShoes.json().then(prod => prod)
-        const requestMensWatches = await fetch('https://dummyjson.com/products/category/mens-watches')
+        const requestMensWatches = await axios.get('https://dummyjson.com/products/category/mens-watches')
         const mensWatchesData = await requestMensWatches.json().then(prod => prod)
-        const requestWomensWatches = await fetch('https://dummyjson.com/products/category/womens-watches')
+        const requestWomensWatches = await axios.get('https://dummyjson.com/products/category/womens-watches')
         const womensWatchesData = await requestWomensWatches.json().then(prod => prod)
-        const requestWomensBags = await fetch('https://dummyjson.com/products/category/womens-bags')
+        const requestWomensBags = await axios.get('https://dummyjson.com/products/category/womens-bags')
         const womensBagsData = await requestWomensBags.json().then(prod => prod)
-        const requestWomensJewellery = await fetch('https://dummyjson.com/products/category/womens-jewellery')
+        const requestWomensJewellery = await axios.get('https://dummyjson.com/products/category/womens-jewellery')
         const womensJewelleryData = await requestWomensJewellery.json().then(prod => prod)
         
         //Home & Furniture
-        const requestHomeDecoration = await fetch('https://dummyjson.com/products/category/home-decoration')
+        const requestHomeDecoration = await axios.get('https://dummyjson.com/products/category/home-decoration')
         const homeDecorationData = await requestHomeDecoration.json().then(prod => prod)
-        const requestFurniture = await fetch('https://dummyjson.com/products/category/furniture')
+        const requestFurniture = await axios.get('https://dummyjson.com/products/category/furniture')
         const furnitureData = await requestFurniture.json().then(prod => prod)
-        const requestLighting = await fetch('https://dummyjson.com/products/category/lighting')
+        const requestLighting = await axios.get('https://dummyjson.com/products/category/lighting')
         const lightingData = await requestLighting.json().then(prod => prod)
 
         //Accessories
-        const requestSunglasses = await fetch('https://dummyjson.com/products/category/sunglasses')
+        const requestSunglasses = await axios.get('https://dummyjson.com/products/category/sunglasses')
         const sunglassesData = await requestSunglasses.json().then(prod => prod)
         
         //SelfCare
-        const requestSkinCare = await fetch('https://dummyjson.com/products/category/skincare')
+        const requestSkinCare = await axios.get('https://dummyjson.com/products/category/skincare')
         const skinCareData = await requestSkinCare.json().then(prod => prod)
         
         //Food & Beverages
-        const requestGroceries = await fetch('https://dummyjson.com/products/category/groceries')
+        const requestGroceries = await axios.get('https://dummyjson.com/products/category/groceries')
         const groceriesData = await requestGroceries.json().then(prod => prod)
 
         //Cars & Parts
-        const requestAutomotive = await fetch('https://dummyjson.com/products/category/automotive')
+        const requestAutomotive = await axios.get('https://dummyjson.com/products/category/automotive')
         const automotiveData = await requestAutomotive.json().then(prod => prod)
-        const requestMotorcycle = await fetch('https://dummyjson.com/products/category/motorcycle')
+        const requestMotorcycle = await axios.get('https://dummyjson.com/products/category/motorcycle')
         const motorcycleData = await requestMotorcycle.json().then(prod => prod)
 
         //Clear
@@ -83,7 +84,7 @@ const populate_data = async () => {
         await AllProductsModel.deleteMany({})
         
         // Insert all the products
-        const requestallData = await fetch('https://dummyjson.com/products?limit=100')
+        const requestallData = await axios.get('https://dummyjson.com/products?limit=100')
         const allData = await requestallData.json().then(prod => prod)
         await AllProductsModel.insertMany(allData.products)
     } catch (error) {
